274a846bfe064a55a30277fc4157829b8dd97bb2
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-09-08 14:14:37 +0100

    boilerplate for imgpoll class

---
+++ src/ImgPool.h
@@ -0,0 +1,29 @@
+#ifndef IMG_POOL_H
+#define IMG_POOL_H
+#include <wx/wx.h>
+#include <memory>
+
+#include "Img.h"
+
+/*
+Stores img objects. The idea is to store 5 images in memory: 
+the current image on image container, an image with a sigma 10% greater, an image with with sigma 20% greatter
+an image with a sigma 10% lower and an image 20% lower, taking the current image as reference
+The idea ia to offer a more responsive experience to user as it moves the seekbar, and to do this, the next two or previous 2
+images must be ready to be show as soon as one of them is required.
+If user moves the seekbar, this buffer has to be updated
+*/
+
+class ImgPool{
+private:
+    std::vector<std::unique_ptr<Img>> _images;
+    Img _original;
+    double currentSigma{ 0 };
+public:
+    wxImage CurrentImage();
+    void AskFor(double sigma);
+
+    ImgPool(std::string imagePath);
+};
+
+#endif
