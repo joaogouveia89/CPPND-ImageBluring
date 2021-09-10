8d90b9470d1bf098b0c88fe919bef19c71fdb0af
Joao Gouveia <j.lucas.gouveia@gmail.com>
2021-09-10 18:23:20 +0100

    parallel working well

--- src/Img.cpp
+++ src/Img.cpp
@@ -9,20 +9,21 @@ Img::Img(std::shared_ptr<cv::Mat> originalImage, double sigma, int width, int he
  }
 
 wxImage Img::toWxBitmap(const int width, const int height) const{
-    cv::Mat toReturn = _result == nullptr ? (*_originalImage.get()) : (*_result.get());
-    cv::cvtColor(toReturn, toReturn, cv::ColorConversionCodes::COLOR_BGR2RGB);
+    cv::Mat toReturnPrev = _result == nullptr ? (*_originalImage.get()) : (*_result.get());
+    cv::Mat toReturn;
+    cv::cvtColor(toReturnPrev, toReturn, cv::ColorConversionCodes::COLOR_BGR2RGB);
     wxImage unhandled(toReturn.cols, toReturn.rows, toReturn.data, true);
     return unhandled.Rescale(width, height, wxIMAGE_QUALITY_HIGH);
 }
 
 void Img::Compute()
 {
-    std::cout << "STARTED\n";
     int filterRatio = 122*_width/_height;
-
-    std::future<void> ftr = std::async(std::launch::async, cv::GaussianBlur, *_originalImage, *_result, cv::Size(filterRatio, filterRatio), _sigma, _sigma, cv::BORDER_DEFAULT);
-    ftr.wait();
+    std::future<cv::Mat> result = std::async([&filterRatio](std::shared_ptr<cv::Mat> original, double sigma) {
+        cv::Mat blurred;
+        cv::GaussianBlur(*original, blurred,  cv::Size(filterRatio, filterRatio), sigma);
+        return blurred;
+    }, _originalImage, Sigma());
     
-    std::cout << "FINISHED\n";
-
+    _result = std::make_shared<cv::Mat>(result.get());
 }
--- src/ImgPool.cpp
+++ src/ImgPool.cpp
@@ -6,7 +6,8 @@ ImgPool::ImgPool(std::string imagePath)
     _originalImage = std::make_shared<cv::Mat>(cv::imread(imagePath));
     if (_originalImage->data != NULL) /* see https://docs.opencv.org/4.5.2/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56 */
     {
-        _images.emplace_back(std::move(std::make_shared<Img>(_originalImage, 0, _inputWidth, _inputHeight)));
+        _images.emplace_back(std::move(std::make_shared<Img>(_originalImage, 0, _originalImage->rows, _originalImage->cols)));
+        _images.emplace_back(std::move(std::make_shared<Img>(_originalImage, 2000, _originalImage->rows, _originalImage->cols)));
     }
     else{
         std::string errorMessage = "Fail to open the file. Check if the file is missing, has improper permissions, is unsupported or an invalid format: check docs for more details: https://docs.opencv.org/4.5.2/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56";
@@ -17,5 +18,9 @@ ImgPool::ImgPool(std::string imagePath)
 
 std::shared_ptr<Img> ImgPool::AskFor(double sigma)
 {
-    return _images.front();
+    if(sigma == 0){
+        return _images.front();
+    }else{
+        return _images.back();
+    }
 }
